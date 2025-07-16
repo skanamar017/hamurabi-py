400412bfdb2429f783f55445fda7f803d9ee9fd6
skanamar017 <skanamar017@gmail.com>
2025-07-16 17:00:50 -0400

    more changes added new methods

--- Hamurabi.py
+++ Hamurabi.py
@@ -22,24 +22,24 @@ class Hammurabi:
 
         max_acres=self.bushels//self.price
         while answer>max_acres:
-            answer=int(input(f"THINK AGAIN, O MIGHTY MASTER. YOU ONLY HAVE ENOUGH BUSHELS TO BUY {max_acres} ACRES. HOW MANY ACRES OF LAND DO YOU PLAN ON BUYING?:"))
+            answer=int(input(f"THINK AGAIN, O MIGHTY MASTER. YOU ONLY HAVE ENOUGH BUSHELS TO BUY {max_acres} ACRES. NOW, THEN, HOW MANY ACRES OF LAND DO YOU PLAN ON BUYING?:"))
         
         self.acres+=answer
         self.bushels-=(answer*self.price)
 
-        return [self.acres, self.bushels]
+        return answer
 
 
     def askHowManyAcresToSell(self, acresOwned):
         answer=int(input("HOW MANY ACRES OF LAND DO YOU PLAN ON SELLING?: "))
         
         while answer>self.acres:
-            answer=int(input(f"THINK AGAIN, O MIGHTY MASTER. YOU ONLY HAVE {self.acres} ACRES. HOW MANY ACRES OF LAND DO YOU PLAN ON SELLING?:"))
+            answer=int(input(f"THINK AGAIN, O MIGHTY MASTER. YOU ONLY HAVE {self.acres} ACRES. NOW, THEN, HOW MANY ACRES OF LAND DO YOU PLAN ON SELLING?:"))
 
-        self.answer-=answer
+        self.acres-=answer
         self.bushels+=(answer*self.price)
         
-        return [self.acres, self.bushels]
+        return answer
 
 
     def askHowMuchGrainToFeedPeople(self, bushels):
@@ -48,9 +48,9 @@ class Hammurabi:
         while answer>self.bushels:
             answer=int(input(f"THINK AGAIN, O MIGHTY MASTER. YOU ONLY HAVE {self.bushels} OF GRAIN. NOW, THEN, HOW MANY BUSHELS OF GRAIN DO YOU WISH TO FEED YOUR PEOPLE?: "))
         
-        bushels_used=self.people*20 #Each person needs at least 20 bushels of grain per year to survive
+        #Each person needs at least 20 bushels of grain per year to survive
 
-        return [bushels_used]
+        return answer
 
     def askHowManyAcresToPlant(self, acresOwned, population, bushels):
         answer=int(input("HOW MANY ACRES DO YOU WISH TO PLANT WITH SEED?: "))
@@ -59,45 +59,63 @@ class Hammurabi:
             while answer>self.bushels//2: #2 bushels to plant 1 acre
                 answer=int(input(f"THINK AGAIN, O MIGHTY MASTER. YOU ONLY HAVE ENOUGH BUSHELS TO PLANT {self.bushels//2} ACRES. NOW, THEN, HOW MANY ACRES DO YOU WISH TO PLANT WITH SEED?: "))
             while answer>self.people*10: #Each person can farm at most 10 acres of land
-                answer=int(input(f"THINK AGAIN, O MIGHTY MASTER. YOU ONLY HAVE ENOUGH PEOPLE TO PLANT {self.people10} ACRES. NOW, THEN, HOW MANY ACRES DO YOU WISH TO PLANT WITH SEED?: "))
+                answer=int(input(f"THINK AGAIN, O MIGHTY MASTER. YOU ONLY HAVE ENOUGH PEOPLE TO PLANT {self.people*10} ACRES. NOW, THEN, HOW MANY ACRES DO YOU WISH TO PLANT WITH SEED?: "))
             while answer>self.acres:
                 answer=int(input(f"THINK AGAIN, O MIGHTY MASTER. YOU ONLY HAVE {self.acres} AVAILIBLE. NOW, THEN, HOW MANY ACRES DO YOU WISH TO PLANT WITH SEED?: "))
 
         self.acres+=(answer)
         self.bushels-=(answer*2)
 
-        return [self.acres, self.bushels]
+        return answer
+
 
 
 
     def plagueDeaths(self, population):
         if random.randint(0, 99) < 15:
             self.people = self.people // 2
-            return self.people
+
         
         
-    def starvationDeaths(population, bushelsFedToPeople):
+    def starvationDeaths(self,population, bushelsFedToPeople):
         #Each person needs 20 bushels of grain to survive. 
         #If you feed them more than this, they are happy, but the grain is still gone. 
         #You don't get any benefit from having happy subjects. 
         #Return the number of deaths from starvation (possibly zero).
-        pass
+        
+        #if more people than needed are fed, deaths are 0
+        #set people to 
+        if self.askHowMuchGrainToFeedPeople()//20>=self.people:
+            return 0
+        self.people-=(self.askHowMuchGrainToFeedPeople()//20)
+        return self.people-(self.askHowMuchGrainToFeedPeople()//20)
 
-    def uprising(population, howManyPeopleStarved):
-        pass
+    def uprising(self, population, howManyPeopleStarved):
+        if self.people/self.starvationDeaths()>=0.45:
+            return True
+        return False
 
-    def immigrants(population, acresOwned, grainInStorage):
-        pass
+    def immigrants(self, population, acresOwned, grainInStorage):
+        if self.starvationDeaths==0:
+            return (20 * self.acres + self.bushels) / (100 * self.bushels) + 1
 
-    def harvest(acres, bushelsUsedAsSeed):
-        pass
+    def harvest(self, acres, bushelsUsedAsSeed):
+        ran_num = random.randint(1, 6)
+        self.bushels+=(ran_num*self.askHowManyAcresToPlant())
+        return ran_num*self.askHowManyAcresToPlant()
 
-    def grainEatenByRats(bushels):
-        pass
 
-    def newCostOfLand():
-        pass
+    def grainEatenByRats(self, bushels):
+        grain_eaten=0
+        ran_num=random.randint(0, 99)
+        if ran_num < 40:
+            grain_eaten=self.bushels*ran_num//100
+        self.bushels-=grain_eaten
+        return grain_eaten
 
+    def newCostOfLand(self):
+        self.price=random.randint(17, 23)
+        return self.price
 
 
 
